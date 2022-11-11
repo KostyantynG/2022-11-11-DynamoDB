bbdc1b9546de834f67a110f871c7031c9c4cbdac
kostyantyn <funyourmind@gmail.com>
2022-11-11 11:10:04 +0100

    update lambda.py

--- terraform/lambda.py
+++ terraform/lambda.py
@@ -1,36 +1,40 @@
 import json
 import requests
 import boto3
-import os
 
-def get_the_jobs():
-    s3_client = boto3.client('s3')
-    
-    url = "https://www.arbeitnow.com/api/job-board-api"
+url = "https://www.arbeitnow.com/api/job-board-api"
+s3_client = boto3.client('s3')
+bucket = "lambda-arbeitnow-challenge"
+
+def get_raw_jobs():    
     response = requests.get(url)
-    raw_data = response.json()
-    jobs_data = raw_data["data"]
-    job = jobs_data[0]
-    job_template = {
-            "id": job['slug'],
-            "title": job['title'],
-            "description": job['description']
-        }
-    
-    for job in jobs_data:
-        job_template = {
-            "id": job['slug'],
-            "title": job['title'],
-            "description": job['description']
-        }
-    
-        job_template_json = json.dumps(job_template)
-          
-        with open("/tmp/job", "w") as file:
-            file.write(job_template_json)
-            
-        s3_client.upload_file('/tmp/job', 'lambda-arbeitnow-challenge', job_template['id'])
-        os.remove("/tmp/job")
+    return response.json()["data"]
+
+def get_jobs_list():
+    raw_jobs = get_raw_jobs()
+    jobs_list = []
+    for job_api in raw_jobs:
+        jobs_list.append({
+            "id": job_api["slug"],
+            "title": job_api["title"],
+            "description": job_api["description"]
+        })
+    return jobs_list
+
+jobs_list = get_jobs_list()
+
+def upload_job(job):
+    job_string = json.dumps(job)
+    job_bytes = str.encode(job_string)
+    s3_client.put_object(
+        Body = job_bytes,
+        Bucket = bucket,
+        Key = job["id"] + ".json"
+    )
+
+def upload_jobs(jobs):
+    for job in jobs:
+        upload_job(job)
         
 def lambda_handler(event, context):
-    get_the_jobs()
+    upload_jobs(jobs_list)
