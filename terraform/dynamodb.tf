4b6b14cb0a78f54d5275b608b3abfdb7b4abf22c
kostyantyn <funyourmind@gmail.com>
2022-11-27 16:47:12 +0100

    edit dynamodb.tf

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -3,6 +3,8 @@ resource "aws_dynamodb_table" "dynamodb-table" {
   hash_key       = "id"    
   read_capacity  = "10"
   write_capacity = "10"
+  stream_enabled = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "id"
