3b08a358d46583e033966a7a2dfc0aaf1973faaa
kostyantyn <funyourmind@gmail.com>
2022-11-28 09:48:15 +0100

    play with for_each

--- terraform/subnet_group.tf
+++ terraform/subnet_group.tf
@@ -1,4 +1,4 @@
 resource "aws_db_subnet_group" "employees_subnet_group" {
     name = "employees subnet group"
-    subnet_ids = ["${aws_subnet.management_a.id}","${aws_subnet.management_b.id}"]
+    subnet_ids = [aws_subnet.management_a.id, aws_subnet.management_b.id]
 }
