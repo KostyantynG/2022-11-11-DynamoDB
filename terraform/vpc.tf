53111b384c2d5330c365affa26dda8b35f7f4414
kostyantyn <funyourmind@gmail.com>
2022-11-28 09:29:47 +0100

    play with for_each

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -89,7 +89,7 @@ resource "aws_subnet" "name" {
 
 for_each = {
   "management_a" = {vpc_id=aws_vpc.scaling_fastapi_vpc.id, cidr_block=["10.0.4.0/24"], availability_zone="us-west-2a"},
-  "management_b" = {vpc_id=aws_vpc.scaling_fastapi_vpc.id, cidr_block=["10.0.5.0/24"], availability_zone="us-west-2a"}
+  "management_b" = {vpc_id=aws_vpc.scaling_fastapi_vpc.id, cidr_block=["10.0.5.0/24"], availability_zone="us-west-2b"}
 }
   vpc_id = each.value.vpc_id
   cidr_block = each.value.cidr_block
