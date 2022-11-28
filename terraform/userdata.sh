69c02b50373724bfa16ddb0adf83c933be04c2e0
kostyantyn <funyourmind@gmail.com>
2022-11-28 08:51:27 +0100

    modify userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -4,4 +4,5 @@ cd /home/ec2-user/
 aws s3 cp s3://arbeitnow-job-bucket/script.zip /home/ec2-user/script.zip
 unzip script.zip
 sudo python3 -m pip install -r requirements.txt
+cd src
 sudo python3 main.py
