6118eeeb8cb67d85c919b8e369108aa25644c655
kostyantyn <funyourmind@gmail.com>
2022-11-28 09:40:11 +0100

    play with for_each

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,4 +1,7 @@
 variable "cidrBlock" {
-    type        = string
-    default     = ["10.0.4.0/24","10.0.5.0/24"]
+    type        = list(string)
+    default     = [
+        "10.0.4.0/24",
+        "10.0.5.0/24"
+    ]
 }
